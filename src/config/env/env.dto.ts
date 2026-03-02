1efd2531f3cec432813d5bda3867c71f3a93d86e
oliveiradniel <kadadniel@gmail.com>
2026-03-02 13:56:08 -0300

    feat(infra): setup PostgreSQL 18 with pgvector via docker-compose
    - provision database container with pgvector extension enabled
    - add database variables in DTO
    - persist data using named volume

--- src/config/env/env.dto.ts
+++ src/config/env/env.dto.ts
@@ -1,14 +1,34 @@
 import { Type } from 'class-transformer';
-import { IsDefined, IsEnum, IsNumber, IsOptional } from 'class-validator';
+import {
+  IsDefined,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
+
 import { NodeEnv } from './types/node-env';
 
 export class EnvironmentVariablesDTO {
   @IsNumber()
-  @IsOptional()
+  @IsNotEmpty()
   @Type(() => Number)
   PORT!: number;
 
   @IsEnum(NodeEnv)
+  @IsNotEmpty()
   @IsDefined()
   NODE_ENV!: NodeEnv;
+
+  @IsString()
+  @IsNotEmpty()
+  POSTGRES_USER!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  POSTGRES_PASSWORD!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  POSTGRES_DB!: string;
 }
